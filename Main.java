0ea7afe94454b9ccdbcc09a2f5940296b50db047
yuanyaos <yuan.yaos@husky.neu.edu>
2019-01-07 00:18:28 -0500

    Problem627_longest_palindrome

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution test = new Solution();
+		String s = "A";
+		int l = test.longestPalindrome(s);
+		System.out.println("LongestPalindrome:"+l);
+	}
+
+}
