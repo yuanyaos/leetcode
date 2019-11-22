cb78f63190b92cc6a78ab029510598df6c67adfc
yaoshenyuan <yuan.yaos@husky.neu.edu>
2019-11-22 17:39:58 -0500

    leetcode14

---
+++ leetcode/leetcode14/Leetcode.java
@@ -0,0 +1,15 @@
+package leetcode14;
+
+public class Leetcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution test=new Solution();
+		String[] strs={"fllllower","fllllow","fllllight"};
+		
+		String prefix;
+		prefix = test.longestCommonPrefix(strs);
+		System.out.println(prefix);
+	}
+
+}
