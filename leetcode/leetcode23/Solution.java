dcb453b1968eff0df7ec64506b2d037ae045ea48
yaoshenyuan <yuan.yaos@husky.neu.edu>
2019-12-03 20:20:17 -0500

    leetcode23 (1)

---
+++ leetcode/leetcode23/Solution.java
@@ -0,0 +1,36 @@
+package leetcode23;
+
+import java.util.*; 
+
+public class Solution {
+	public ListNode mergeKLists(ListNode[] lists) {
+		if(lists==null)
+			return null;
+		
+		PriorityQueue<ListNode> que = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>(){
+			public int compare(ListNode a, ListNode b) 
+		    { 
+		        return a.val-b.val; 
+		    }
+		});
+		
+		for(ListNode l:lists){
+			if(l!=null)
+				que.offer(l);
+		}
+		
+		ListNode head = new ListNode(0);
+		ListNode p = head;
+		
+		while(!que.isEmpty()){
+			ListNode qout = que.poll();
+			p.next = qout;
+			p = p.next;
+			
+			if(qout.next!=null)
+				que.offer(qout.next);
+		}
+		
+		return head.next;
+    }
+}
---
+++ leetcode/leetcode23/leetcode.java
@@ -0,0 +1,26 @@
+package leetcode23;
+
+import java.util.*; 
+
+public class leetcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution test=new Solution();
+		
+		ListNode[] lists = new ListNode[3];
+		
+		for(int i=0;i<lists.length;i++){
+			lists[i] = 
+		}
+		
+		List<String> result = new ArrayList<String>();
+		int n=3;
+		result = test.generateParenthesis(n);
+		for(int i=0;i<result.size();i++){
+			System.out.println(result.get(i));
+			System.out.println();
+		}
+	}
+
+}
