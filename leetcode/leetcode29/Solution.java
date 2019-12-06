75d002f9ed8ba4bd1980636e82ac848177bf7432
yaoshenyuan <yuan.yaos@husky.neu.edu>
2019-12-06 12:11:46 -0500

    leetcode29 (2)

--- leetcode/leetcode29/Solution.java
+++ leetcode/leetcode29/Solution.java
@@ -4,23 +4,27 @@ public class Solution {
 	public int divide(int dividend, int divisor) {
 		if(divisor==0)
 			return Integer.MAX_VALUE;
+		if(divisor==-1 && dividend == Integer.MIN_VALUE)
+	        return Integer.MAX_VALUE;
 		
-		long dividend_abs = Math.abs(dividend);
-		long divisor_abs = Math.abs(divisor);
+		long dividend_abs = Math.abs((long)dividend);
+		long divisor_abs = Math.abs((long)divisor);
 		
 		long result = 0;
-		while(dividend_abs>divisor_abs){
+		long resultt = 0;
+		long mul;
+		while(dividend_abs>=divisor_abs){
 			
 			int pow = 0;
-			long mul = divisor_abs<<pow;
-			while(dividend_abs>mul){
+			mul = divisor_abs<<pow;
+			while(dividend_abs>=mul){
 				pow = pow+1;
 				mul = divisor_abs<<pow;
 			}
-			mul = 1<<(pow-1);	// Notice here
-			divisor_abs = divisor_abs-mul;
-			
-			result = result+mul;
+			mul = divisor_abs<<(pow-1);
+			resultt = 1<<(pow-1);
+			result = result+resultt;	// Notice here
+			dividend_abs = dividend_abs-mul;
 		}
 		
 		if((dividend>0 && divisor>0) || (dividend<0 && divisor<0))
---
+++ leetcode/leetcode29/leetcode.java
@@ -0,0 +1,18 @@
+package leetcode29;
+
+public class leetcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		-2147483648
+//		-1
+		int dividend = -2147483648;
+		int divisor = 1;
+		
+		Solution test = new Solution();
+		int result = test.divide(dividend, divisor);
+		
+		System.out.println(result);
+	}
+
+}
