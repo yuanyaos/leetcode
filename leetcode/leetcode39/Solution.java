cd4a883f7bb29169465f808437431c47f8bd4a04
yaoshenyuan <yuan.yaos@husky.neu.edu>
2019-12-11 13:24:52 -0500

    leetcode39 (2)

--- leetcode/leetcode39/Solution.java
+++ leetcode/leetcode39/Solution.java
@@ -3,18 +3,19 @@ package leetcode39;
 import java.util.*;
 
 public class Solution {
-	private void DFSsearch(List<List<Integer>> result, ArrayList<Integer> comb, int[] candidates, int sum, int target){
-		if(sum==target){
-			result.add(comb);
-			return;
-		}
+	private void DFSsearch(List<List<Integer>> result, List<Integer> comb, int[] candidates, int sum, int target, int start){ // add start index to avoid repeated list
 		if(sum>target)
 			return;
 		
-		for(int i=0;i<candidates.length;i++){
+		if(sum==target){
+			result.add(new ArrayList<>(comb));
+			return;
+		}
+		
+		for(int i=start;i<candidates.length;i++){
 			comb.add(candidates[i]);
 			sum = sum+candidates[i];
-			DFSsearch(result, comb, candidates, sum, target);
+			DFSsearch(result, comb, candidates, sum, target,i);
 			comb.remove(comb.size()-1);
 			sum = sum-candidates[i];
 		}
@@ -22,10 +23,10 @@ public class Solution {
 	}
 	
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		List<List<Integer>> combination = new List<ArrayList<Integer>>();
-		ArrayList<Integer> comb = new ArrayList<Integer>();
+		List<List<Integer>> combination = new ArrayList<>();
+		List<Integer> comb = new ArrayList<>();
 		int sum = 0;
-		DFSsearch(combination,comb,candidates,sum,target);
+		DFSsearch(combination,comb,candidates,sum,target,0);
 		
 		return combination;
     }
---
+++ leetcode/leetcode39/leetcode.java
@@ -0,0 +1,19 @@
+package leetcode39;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		List<List<Integer>> combinationSum(int[] candidates, int target)
+		
+		int[] candidates = new int[]{2,3,6,7};
+		int target = 7;
+		
+		Solution test = new Solution();
+		List<List<Integer>> result = test.combinationSum(candidates, target);
+	}
+
+}
