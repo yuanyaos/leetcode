03e1302c0f7b9f47cc3a8251c9050faf1ab14572
yaoshenyuan <yuan.yaos@husky.neu.edu>
2019-12-19 17:44:23 -0500

    leetcode70

---
+++ leetcode/leetcode70/leetcode.java
@@ -0,0 +1,17 @@
+package leetcode70;
+
+public class leetcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 2;
+		
+		Solution test = new Solution();
+		
+		int result = test.climbStairs(n);
+		
+		System.out.println(result);
+	}
+
+}
+//1134903170
